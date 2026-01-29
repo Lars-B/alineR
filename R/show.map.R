55401640cd87084655c4102320f1697daef72d85
Lars-B <berlinglars96@gmail.com>
2026-01-28 16:22:22 -0800

    fixing show.map()

--- R/show.map.R
+++ R/show.map.R
@@ -1,7 +1,17 @@
 show.map<-function(){
-  map<-read.csv("./data/aline_mapset_workingcopy.csv")
-  # map<-read.csv("./data/aline_diacritic_set.csv")
-  show.map<-cbind(apply(data.frame(map$U.Val),MARGIN=1,FUN=intToUtf8),map,deparse.level=0)
-  colnames(show.map)[1]<-"IPA"
+  map.file <- system.file("extdata", "aline_mapset_workingcopy.csv", package="alineR")
+  if (map.file == "") {
+    stop("Required ALINE map file is missing form the package!")
+  }
+
+  map<-read.csv(map.file)
+
+  show.map<-cbind(
+    apply(data.frame(map$U.Val), MARGIN = 1, FUN = intToUtf8),
+    map,
+    deparse.level = 0
+    )
+
+  colnames(show.map)[1] <- "IPA"
   return(show.map)
 }
